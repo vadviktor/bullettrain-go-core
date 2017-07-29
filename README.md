7a864c903846b8d9a8bb3083e8ddcbfc7eeca644
Viktor (Icon) VAD <vad.viktor@gmail.com>
2017-07-29 21:29:00 +0100

    Removing go dep tool. Switching over to channels and goroutines.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [DRAFT]
+# !!! DOCUMENTATION IS HEAVILY EVOLVING
 
 Shell prompt made from building blocks while staying styleish.
 
@@ -45,4 +45,18 @@ but to reevaluate on every call.
 
 # Development
 
-Use https://github.com/golang/dep
+We not only want the prompt to be super sexy but also super snappy. What'd be the point writting it in Go?! :)
+
+So to bluntly benchmark it's speed, build the executable and then sample a 10x batch 5 times like this in ZSH:
+
+```
+$ go build bullettrain.go
+$ repeat 5 (time (repeat 10 ./bullettrain > /dev/null))
+( repeat 10; do; ./bullettrain > /dev/null; done; )  0.48s user 0.16s system 107% cpu 0.590 total
+( repeat 10; do; ./bullettrain > /dev/null; done; )  0.48s user 0.14s system 107% cpu 0.581 total
+( repeat 10; do; ./bullettrain > /dev/null; done; )  0.51s user 0.15s system 107% cpu 0.615 total
+( repeat 10; do; ./bullettrain > /dev/null; done; )  0.49s user 0.17s system 107% cpu 0.613 total
+( repeat 10; do; ./bullettrain > /dev/null; done; )  0.51s user 0.17s system 107% cpu 0.625 total
+```
+
+Be sure to benchmark your code to make sure you are not introducing a feature that will make the prompt sluggish all of a sudden.
--- bullettrain.go
+++ bullettrain.go
@@ -12,18 +12,33 @@ func main() {
 	color.NoColor = false // force terminal to use colours
 	const lineEnding = "$"
 
-	segmentList := getSegments()
-	for _, segment := range segmentList {
-		fmt.Print(segment.Render())
+	var segmentList []renderer = getSegments()
+
+	// Create a channel for each segment.
+	chans := make([]chan string, len(segmentList))
+	for i := range chans {
+		chans[i] = make(chan string)
+	}
+
+	// Spin off a goroutine for each segment.
+	for i, segment := range segmentList {
+		go segment.Render(chans[i])
+	}
+
+	// Gather each goroutine's response through their channels,
+	// keeping their order.
+	for _, c := range chans {
+		fmt.Print(<-c)
 	}
 
 	fmt.Printf("\n%s x", color.HiGreenString(lineEnding))
 }
 
 type renderer interface {
-	Render() string
+	Render(c chan<- string)
 }
 
+// Configure the segments and store them in the right order.
 func getSegments() []renderer {
 	python := &bullettrain_go_python.PythonSegment{}
 	python.SetFg(color.FgHiWhite)
@@ -37,22 +52,40 @@ func getSegments() []renderer {
 	}
 }
 
+//  _____                            _
+// /  ___|                          | |
+// \ `--.  ___ _ __   __ _ _ __ __ _| |_ ___  _ __
+//  `--. \/ _ \ '_ \ / _` | '__/ _` | __/ _ \| '__|
+// /\__/ /  __/ |_) | (_| | | | (_| | || (_) | |
+// \____/ \___| .__/ \__,_|_|  \__,_|\__\___/|_|
+//            | |
+//            |_|
+
 type separator struct {
 	fg, bg color.Attribute
 }
 
-func (s *separator) Render() string {
+func (s *separator) Render(ch chan<- string) {
 	const segmentSeparator string = ""
-	c := color.New(s.fg, s.bg)
-	return c.Sprint(segmentSeparator)
+	col := color.New(s.fg, s.bg)
+	ch <- col.Sprint(segmentSeparator)
+	close(ch)
 }
+
+//  _____ _
+// |_   _(_)
+//   | |  _ _ __ ___   ___
+//   | | | | '_ ` _ \ / _ \
+//   | | | | | | | | |  __/
+//   \_/ |_|_| |_| |_|\___|
 
 type timeSegment struct {
 	fg, bg color.Attribute
 }
 
-func (s *timeSegment) Render() string {
-	c := color.New(s.fg, s.bg)
+func (s *timeSegment) Render(ch chan<- string) {
+	col := color.New(s.fg, s.bg)
 	t := time.Now()
-	return c.Sprintf(" %02d:%02d:%02d ", t.Hour(), t.Minute(), t.Second())
+	ch <- col.Sprintf(" %02d:%02d:%02d ", t.Hour(), t.Minute(), t.Second())
+	close(ch)
 }
