88621fb07177ebd472243a5045b335146500fee2
higan <higan@live.cn>
2019-05-28 21:17:46 +0800

    Update tests

--- src/car/directory/dir_test.go
+++ src/car/directory/dir_test.go
@@ -2,6 +2,7 @@ package carDirectory
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -9,32 +10,14 @@ const failTpl = "Unexpected directory format.\nEXPECTED: %s\nGOT:      %s"
 
 func setup() {
 	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_PATH_SEPARATOR", "")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "3")
 	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_DEPTH_INDICATOR", "...")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "true")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "true")
 }
 
 func TestFullPath(t *testing.T) {
 	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "0")
-	directory := "/usr/share/doc/vpn/easy"
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FRONT_MAX_LENGTH", "-1")
+	directory := filepath.Clean("/usr/share/doc/vpn/easy")
 	expected := "usrsharedocvpneasy"
-
-	actual := rebuildDirForRender(directory)
-
-	if actual != expected {
-		t.Logf(failTpl, expected, actual)
-		t.Fail()
-	}
-}
-
-func TestWithoutRoot(t *testing.T) {
-	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "0")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usrsharedocvpneasy"
 
 	actual := rebuildDirForRender(directory)
 
@@ -48,8 +31,8 @@ func TestCustomPathSeparator(t *testing.T) {
 	setup()
 	sep := "|"
 	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_PATH_SEPARATOR", sep)
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "5")
-	directory := "/usr/share/doc/vpn/easy"
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FRONT_MAX_LENGTH", "-1")
+	directory := filepath.Clean("/usr/share/doc/vpn/easy")
 	expected := "|usr|share|doc|vpn|easy"
 
 	actual := rebuildDirForRender(directory)
@@ -60,13 +43,13 @@ func TestCustomPathSeparator(t *testing.T) {
 	}
 }
 
-func TestFirstDirShowingLength2(t *testing.T) {
+func TestMergeMode(t *testing.T) {
 	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "2")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "true")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usr...easy"
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FRONT_MAX_LENGTH", "2")
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_TAIL_MAX_LENGTH", "1")
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ACRONYM_MODE", "merge")
+	directory := filepath.Clean("/usr/share/doc/vpn/easy")
+	expected := "usr...easy"
 
 	actual := rebuildDirForRender(directory)
 
@@ -76,13 +59,12 @@ func TestFirstDirShowingLength2(t *testing.T) {
 	}
 }
 
-func TestFirstDirNotShowingLength2(t *testing.T) {
+func TestAcronymMode(t *testing.T) {
 	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "2")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "false")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "...vpneasy"
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FRONT_MAX_LENGTH", "2")
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_TAIL_MAX_LENGTH", "1")
+	directory := filepath.Clean("/usr/share/doc/vpn/easy")
+	expected := "usrs*d*v*easy"
 
 	actual := rebuildDirForRender(directory)
 
@@ -92,93 +74,13 @@ func TestFirstDirNotShowingLength2(t *testing.T) {
 	}
 }
 
-func TestFirstDirNotShowingFullLength(t *testing.T) {
+func TestCustomAcronymSymbol(t *testing.T) {
 	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "0")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "false")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usrsharedocvpneasy"
-
-	actual := rebuildDirForRender(directory)
-
-	if actual != expected {
-		t.Logf(failTpl, expected, actual)
-		t.Fail()
-	}
-}
-
-func TestFirstDirNotShowingLength1(t *testing.T) {
-	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "1")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "false")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "...easy"
-
-	actual := rebuildDirForRender(directory)
-
-	if actual != expected {
-		t.Logf(failTpl, expected, actual)
-		t.Fail()
-	}
-}
-
-func TestFirstDirShowingFullLength(t *testing.T) {
-	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "0")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "true")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usrsharedocvpneasy"
-
-	actual := rebuildDirForRender(directory)
-
-	if actual != expected {
-		t.Logf(failTpl, expected, actual)
-		t.Fail()
-	}
-}
-
-func TestFirstDirShowingLength1(t *testing.T) {
-	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "1")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "true")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usr...easy"
-
-	actual := rebuildDirForRender(directory)
-
-	if actual != expected {
-		t.Logf(failTpl, expected, actual)
-		t.Fail()
-	}
-}
-
-func TestFirstDirShowingLength3(t *testing.T) {
-	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "3")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "true")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usr...vpneasy"
-
-	actual := rebuildDirForRender(directory)
-
-	if actual != expected {
-		t.Logf(failTpl, expected, actual)
-		t.Fail()
-	}
-}
-
-func TestFirstDirNotShowingLength3(t *testing.T) {
-	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "3")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "false")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "...docvpneasy"
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ELLIPSIS", "~")
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FRONT_MAX_LENGTH", "2")
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_TAIL_MAX_LENGTH", "1")
+	directory := filepath.Clean(filepath.Clean("/usr/share/doc/vpn/easy"))
+	expected := "usrs~d~v~easy"
 
 	actual := rebuildDirForRender(directory)
 
@@ -190,12 +92,10 @@ func TestFirstDirNotShowingLength3(t *testing.T) {
 
 func TestCustomDepthIndicator(t *testing.T) {
 	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "3")
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ACRONYM_MODE", "merge")
 	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_DEPTH_INDICATOR", "+++")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_ROOT_SHOW", "false")
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FIRST_DIR_SHOW", "true")
-	directory := "/usr/share/doc/vpn/easy"
-	expected := "usr+++vpneasy"
+	directory := filepath.Clean(filepath.Clean("/usr/share/doc/vpn/easy"))
+	expected := "usr+++vpneasy"
 
 	actual := rebuildDirForRender(directory)
 
@@ -207,10 +107,10 @@ func TestCustomDepthIndicator(t *testing.T) {
 
 func TestHomeDirectoryShorthand(t *testing.T) {
 	setup()
-	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_MAX_LENGHT", "5")
-	os.Setenv("HOME", "/home/ikon")
-	directory := "/home/ikon/doc/vpn/easy"
-	expected := "~docvpneasy"
+	os.Setenv("BULLETTRAIN_CAR_DIRECTORY_FRONT_MAX_LENGTH", "-1")
+	os.Setenv("HOME", filepath.Clean("/home/ikon"))
+	directory := filepath.Clean("/home/ikon/doc/vpn/easy")
+	expected := "~docvpneasy"
 
 	actual := rebuildDirForRender(directory)
 
